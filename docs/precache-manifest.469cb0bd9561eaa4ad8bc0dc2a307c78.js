a407052c3f6738a42d35dbbceeccc3e24c3347a7
Mario <mariopw4@gmail.com>
2020-07-21 13:29:25 -0500

    Docs agregado

---
+++ docs/precache-manifest.469cb0bd9561eaa4ad8bc0dc2a307c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fad5772a0d5bff419c8afa1790ea1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5992e37cc4c00b6565a",
+    "url": "/static/css/main.a16ff129.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5992e37cc4c00b6565a",
+    "url": "/static/js/main.1aa5dbff.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
